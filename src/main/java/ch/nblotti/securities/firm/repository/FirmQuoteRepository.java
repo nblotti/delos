6936e5f049471040d9eb3cccae0a8e9930bd23e8
Nicholas Blotti <nblotti@gmail.com>
2020-05-14 17:23:39 +0200

    data rest access

--- src/main/java/ch/nblotti/securities/firm/repository/FirmQuoteRepository.java
+++ src/main/java/ch/nblotti/securities/firm/repository/FirmQuoteRepository.java
@@ -2,8 +2,15 @@ package ch.nblotti.securities.firm.repository;
 
 import ch.nblotti.securities.firm.to.FirmEODQuoteTO;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FirmQuoteRepository extends CrudRepository<FirmEODQuoteTO, Long> {
+import java.time.LocalDate;
+import java.util.Collection;
+
+@RepositoryRestResource(path = "firmquotes")
+public interface FirmQuoteRepository  extends PagingAndSortingRepository<FirmEODQuoteTO, Integer> {
+
+  public Collection<FirmEODQuoteTO> findAllByDate(LocalDate date);
 }
