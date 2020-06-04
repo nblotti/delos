dfe6f55016ea3732c9b23905adf9e9cd71e3b905
Nicholas Blotti <nblotti@gmail.com>
2020-06-04 13:41:46 +0200

    added sector & industry to mat views

--- src/main/resources/db/migration/V1_1_7__vue_mat_top_movers_gainers_loosers.sql
+++ src/main/resources/db/migration/V1_1_7__vue_mat_top_movers_gainers_loosers.sql
@@ -4,10 +4,12 @@ DROP
 MATERIALIZED VIEW  IF EXISTS mv_movers;
 create materialized view mv_movers as
 select
-       ROW_NUMBER () OVER (ORDER BY u.code asc) id ,
+    ROW_NUMBER () OVER (ORDER BY u.code asc) id ,
        u.date,
        u.code,
        u.exchange,
+       u.sector,
+       u.industry,
        u.CURRENT_EXCHANGE,
        u.name,
        u.type,
@@ -19,25 +21,25 @@ select
        t.volume,
        t.last_move
 from (
-         select r.*,
-                CASE
-                    WHEN cast(r.previous_adjusted_close as numeric) < 1 THEN 0
-                    ELSE ROUND(cast((((r.adjusted_close - r.previous_adjusted_close) / r.previous_adjusted_close) *
-                                     100) as numeric), 2) END last_move,
-                RANK() OVER (partition by code
-         order by date) rank from (
-   with cte AS (
+    select r.*,
+    CASE
+    WHEN cast(r.previous_adjusted_close as numeric) < 1 THEN 0
+    ELSE ROUND(cast((((r.adjusted_close - r.previous_adjusted_close) / r.previous_adjusted_close) *
+    100) as numeric), 2) END last_move,
+    RANK() OVER (partition by code
+    order by date) rank from (
+    with cte AS (
     select * from (
-        select *, RANK() OVER (partition by code order by date desc) rank_number from firm_eod_quote where code in (select distinct fi.code from FIRM_EOD_SHARE_STATS fs, firm_eod_info fi where
-     fs.shares_outstanding != 0 and fi.type = 'Common Stock' and fs.code = fi.code and fs.exchange = fi.exchange)
+    select *, RANK() OVER (partition by code order by date desc) rank_number from firm_eod_quote where code in (select distinct fi.code from FIRM_EOD_SHARE_STATS fs, firm_eod_info fi where
+    fs.shares_outstanding != 0 and fi.type = 'Common Stock' and fs.code = fi.code and fs.exchange = fi.exchange)
     ) s where rank_number = 1 or rank_number = 5 order by code, rank_number desc
-   )
-   select date, code, exchange, adjusted_close, LAG (adjusted_close,1) over (order by code) previous_adjusted_close , volume from cte
-   ) r
- )t,
- (
-select *, RANK() OVER (partition by code order by date desc) rank_number
-from firm_eod_info ) u
+    )
+    select date, code, exchange, adjusted_close, LAG (adjusted_close,1) over (order by code) previous_adjusted_close , volume from cte
+    ) r
+    )t,
+    (
+    select *, RANK() OVER (partition by code order by date desc) rank_number
+    from firm_eod_info ) u
 where t.rank = 2
   and
     u.rank_number = 1
@@ -48,7 +50,7 @@ where t.rank = 2
 /*-------------------------------------------------*/
 
 create materialized view mv_movers_volume as
-select m.id, m.date, m.code, m.exchange, m.current_exchange, m.name, m.type, m.isin, m.cusip, m.updated_at, m.adjusted_close, m.previous_adjusted_close,t.volume,m.last_move
+select m.id, m.date, m.code, m.exchange, m.current_exchange, m.sector,m.industry, m.name, m.type, m.isin, m.cusip, m.updated_at, m.adjusted_close, m.previous_adjusted_close,t.volume,m.last_move
 from (
          select code, exchange, name, sum(volume) as volume
          from (
