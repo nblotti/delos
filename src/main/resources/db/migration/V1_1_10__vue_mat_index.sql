df385ab00cffc86db418dca12075ec5e6e361fa7
Nicholas Blotti <nblotti@gmail.com>
2020-06-09 21:40:52 +0200

    index mv

--- src/main/resources/db/migration/V1_1_10__vue_mat_index.sql
+++ src/main/resources/db/migration/V1_1_10__vue_mat_index.sql
@@ -3,17 +3,17 @@ MATERIALIZED VIEW  IF EXISTS mv_movers_index;
 
 create
 materialized view mv_movers_index as
-select *, adjusted_close - previous_adjusted_close last_move
+select  ROW_NUMBER() OVER (order by code) row_number, date,code, volume, adjusted_close, previous_adjusted_close, adjusted_close - previous_adjusted_close last_move
 from (
-         select *, RANK() OVER ( partition by code
-         order by date desc) rank from (
-            with cte AS (
-                select * from ( select *, RANK() OVER (partition by code order by date desc) rank_number from index_eod_quote  ) r
-                where rank_number = 1 or rank_number = 5 order by code, rank_number desc
-            )
-            select date, code,  adjusted_close, LAG (adjusted_close,1) over (order by code) previous_adjusted_close , volume from cte
-        )  t
-) u
+    select *, RANK() OVER ( partition by code
+    order by date desc) rank from (
+    with cte AS (
+    select * from ( select *, RANK() OVER (partition by code order by date desc) rank_number from index_eod_quote  ) r
+    where rank_number = 1 or rank_number = 5 order by code, rank_number desc
+    )
+    select date, code,  adjusted_close, LAG (adjusted_close,1) over (order by code) previous_adjusted_close , volume from cte
+    )  t
+    ) u
 where rank = 1
 order by code, date desc;
 
