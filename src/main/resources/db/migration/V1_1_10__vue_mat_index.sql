8708b26820b110f5d4836a5d51e9d76a0755afd0
Nicholas Blotti <nblotti@gmail.com>
2020-06-16 09:23:59 +0200

    two version of index movers

--- src/main/resources/db/migration/V1_1_10__vue_mat_index.sql
+++ src/main/resources/db/migration/V1_1_10__vue_mat_index.sql
@@ -3,7 +3,21 @@ MATERIALIZED VIEW  IF EXISTS mv_movers_index;
 
 create
 materialized view mv_movers_index as
-select  ROW_NUMBER() OVER (order by code) row_number, date,code, volume, adjusted_close, previous_adjusted_close, adjusted_close - previous_adjusted_close last_move
+(select  ROW_NUMBER() OVER (order by code) row_number, 1 as type, date,code, volume, adjusted_close, previous_adjusted_close, adjusted_close - previous_adjusted_close last_move
+from (
+    select *, RANK() OVER ( partition by code
+    order by date desc) rank from (
+    with cte AS (
+    select * from ( select *, RANK() OVER (partition by code order by date desc) rank_number from index_eod_quote  ) r
+    where rank_number = 1 or rank_number = 2 order by code, rank_number desc
+    )
+    select date, code,  adjusted_close, LAG (adjusted_close,1) over (order by code) previous_adjusted_close , volume from cte
+    )  t
+    ) u
+where rank = 1
+order by code, date desc)
+UNION ALL
+(select  ROW_NUMBER() OVER (order by code) row_number, 5 as type, date,code, volume, adjusted_close, previous_adjusted_close, adjusted_close - previous_adjusted_close last_move
 from (
     select *, RANK() OVER ( partition by code
     order by date desc) rank from (
@@ -15,7 +29,8 @@ from (
     )  t
     ) u
 where rank = 1
-order by code, date desc;
+order by code, date desc) order by date, code;
+
 
 drop
 FUNCTION IF EXISTS refresh_fn CASCADE;
